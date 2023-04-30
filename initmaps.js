a5c5855178b65e8638095233056919f344b399f7
Ishmael <user.email 712020:fad7fbb1-7227-4c95-9a49-2a24ff4e7bf5@bots.bitbucket.org>
2023-04-30 10:58:57 +0200

    zoom = 14

--- initmaps.js
+++ initmaps.js
@@ -5,7 +5,7 @@ function initMaps() {
     };
     // Initialize collection map
     var collectionMap = new google.maps.Map(document.getElementById("collect-map"), {
-        zoom: 13.5,
+        zoom: 14,
         center: {
             lat: location.lat + 0.002,
             lng: location.lng
