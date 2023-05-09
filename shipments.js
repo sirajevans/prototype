14b7f492ae402e5dcc8de63dd4e4e176aaff1088
siraj <sirajevans@gmail.com>
2023-05-09 15:20:43 +0200

    incl: jq for toggles

--- shipments.js
+++ shipments.js
@@ -294,13 +294,8 @@ $("#filter_btn").on("click", function () {
 
 // toggle accordions on accordion-header click
 $('.accordion-header').on('click', function () {
-    // $(this).parent().toggleClass('open');
-    // $(this).find('.accordion-arrow').toggleClass('active');
-    // $(this).siblings('.accordion-body').toggleClass('active');
-
-    $('.accordion-body').css('opacity', 0);
-    $('.accordion').addClass('open');
-    const height = $('.accordion-body').height() + 'px';
-    accordion.height(height);
+    $(this).parent().toggleClass('open');
+    $(this).find('.accordion-arrow').toggleClass('active');
+    $(this).siblings('.accordion-body').toggleClass('active');
 });
 
