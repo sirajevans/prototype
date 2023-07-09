2f2f4419b114b5a83c41318c9de756fa343c0485
siraj <sirajevans@gmail.com>
2023-07-09 23:31:11 +0200

    update

--- orders.js
+++ orders.js
@@ -170,3 +170,10 @@ $("#filter_btn").on("click", function () {
         $("#filter_side_modal").addClass("side-modal-active");
     }, 1);
 });
+
+// toggle accordions on accordion-header click
+$('.accordion-header').on('click', function () {
+    $(this).parent().toggleClass('open');
+    $(this).find('.accordion-arrow').toggleClass('active');
+    $(this).siblings('.accordion-body').toggleClass('active');
+});
