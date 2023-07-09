93c1439548141ffe5d504c10f9f2044215875cd6
siraj <sirajevans@gmail.com>
2023-07-09 22:28:41 +0200

    update

--- orders.js
+++ orders.js
@@ -161,3 +161,12 @@ function openSideMenu() {
         }, 150);
     });
 });
+
+// filter side modal
+$("#filter_btn").on("click", function () {
+    $("#filter_modal_container").css("display", "flex");
+    setTimeout(() => {
+        $("#filter_modal_container").addClass("side-modal-container-active");
+        $("#filter_side_modal").addClass("side-modal-active");
+    }, 1);
+});
