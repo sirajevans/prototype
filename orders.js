b33bad80a3a5cd1703367e1193db7f50c4da25c7
siraj <sirajevans@gmail.com>
2023-07-09 17:07:40 +0200

    updated js

--- orders.js
+++ orders.js
@@ -100,3 +100,18 @@ $('.parcels-accordion-header').on('click', function () {
     $(this).find('.accordion-arrow').toggleClass('active');
     $(this).siblings('.parcels-accordion-body').toggleClass('active');
 });
+
+/* Side menu animation JS */
+function openSideMenu() {
+    document.getElementById("side-menu").style.left = "0%";
+    document.getElementById("overlay").style.transitionTimingFunction = "ease";
+    document.getElementById("overlay").style.transitionDuration = "0ms";
+    document.getElementById("overlay").style.zIndex = "1000";
+      setTimeout(() => {  document.getElementById("overlay").style.transitionDuration = "250ms";
+    document.getElementById("overlay").style.opacity = "100%"; }, 10); /* Delay opacity to give zindex time to change from -1 - 1000 wwhile opacity is still at 0*/
+  }
+  function closeSideMenu() {
+    document.getElementById("side-menu").style.left = "-370px";
+    document.getElementById("overlay").style.opacity = "0%";
+      setTimeout(() => {  document.getElementById("overlay").style.zIndex = "-1"; }, 10); /* Delay zIndex so opacity changes to 0 before zindex is set to -1 */
+  }
