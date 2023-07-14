24d71a32099106fa16b3dc9bd48662897d1bf3cd
siraj <sirajevans@gmail.com>
2023-07-14 12:52:20 +0200

    removed unnecessary files

--- team.js
+++
@@ -1,8 +0,0 @@
-/* Add team member */
-$("#add_team_btn").on("click", function () {
-    $("#team_modal_container").css("display", "flex");
-    setTimeout(() => {
-        $("#team_modal_container").addClass("center-modal-container-active");
-        $("#team_center_modal").addClass("center-modal-active");
-    }, 1);
-});
