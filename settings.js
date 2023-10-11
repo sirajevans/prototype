f8ca021d061fd5519fd9574b15abb58497ad7aa0
siraj <sirajevans@gmail.com>
2023-10-11 12:38:48 +0200

    updated js to include notification tab functionality

--- settings.js
+++ settings.js
@@ -3,27 +3,27 @@ const togglePassword = $("#togglePass");
 const password = $("#resetPass");
 
 togglePassword.on("click", function () {
-    // toggle the type attribute
-    const type = password.attr("type") === "password" ? "text" : "password";
-    password.attr("type", type);
+  // toggle the type attribute
+  const type = password.attr("type") === "password" ? "text" : "password";
+  password.attr("type", type);
 
-    // toggle the icon
-    $(this).toggleClass("eye-open");
+  // toggle the icon
+  $(this).toggleClass("eye-open");
 });
 
 // prevent form submit
 const form = $("form");
 form.on("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 });
 
 // PAYMENTS
 // card stacking
 let cards = document.getElementsByClassName("payment-card")
 for (let i = 0; i < cards.length; i++) {
-    let card = cards[i]
-    let leftPositioning = i * 20
-    card.style.left = `${leftPositioning}px`
+  let card = cards[i]
+  let leftPositioning = i * 20
+  card.style.left = `${leftPositioning}px`
 }
 // predetermine how many cards are in the stack
 let length = cards.length
@@ -34,35 +34,35 @@ let cardStackPadding = 30;
 let defaultCardWidth = 165;
 // Dynamically set the indentation for each card in the stack 
 for (let index = 0; index < length; index++) {
-    cards[index].style.left = `${index * cardStackIndent}px`;
+  cards[index].style.left = `${index * cardStackIndent}px`;
 }
 try {
-    let numberOfCards = length > 1 ? length - 1 : 1;
-    let cardWidth = cards ? cards[0].offsetWidth : defaultCardWidth;
-    let cardStackWidth = cardWidth + (cardStackIndent * numberOfCards)
-    let addPaymentWrapper = document.getElementsByClassName("add-payment-wrapper")[0]
-    addPaymentWrapper.style.setProperty("position", "sticky");
-    addPaymentWrapper.style.setProperty("left", `${cardStackWidth + cardStackPadding}px`);
-    addPaymentWrapper.style.setProperty("width", `calc(100% - ${cardStackWidth + cardStackPadding - cardStackIndent}px)`);
+  let numberOfCards = length > 1 ? length - 1 : 1;
+  let cardWidth = cards ? cards[0].offsetWidth : defaultCardWidth;
+  let cardStackWidth = cardWidth + (cardStackIndent * numberOfCards)
+  let addPaymentWrapper = document.getElementsByClassName("add-payment-wrapper")[0]
+  addPaymentWrapper.style.setProperty("position", "sticky");
+  addPaymentWrapper.style.setProperty("left", `${cardStackWidth + cardStackPadding}px`);
+  addPaymentWrapper.style.setProperty("width", `calc(100% - ${cardStackWidth + cardStackPadding - cardStackIndent}px)`);
 } catch (error) {
-    console.error("", error)
+  console.error("", error)
 }
 
 // add card modal
 $("#add_card_btn").on("click", function () {
-    $("#add_card_modal_container").css("display", "flex");
-    setTimeout(() => {
-        $("#add_card_modal_container").addClass("center-modal-container-active");
-        $("#add_card_center_modal").addClass("center-modal-active");
-    }, 1);
+  $("#add_card_modal_container").css("display", "flex");
+  setTimeout(() => {
+    $("#add_card_modal_container").addClass("center-modal-container-active");
+    $("#add_card_center_modal").addClass("center-modal-active");
+  }, 1);
 });
 
 // eft modal
 $("#eft_btn").on("click", function () {
   $("#eft_modal_container").css("display", "flex");
   setTimeout(() => {
-      $("#eft_modal_container").addClass("center-modal-container-active");
-      $("#eft_center_modal").addClass("center-modal-active");
+    $("#eft_modal_container").addClass("center-modal-container-active");
+    $("#eft_center_modal").addClass("center-modal-active");
   }, 1);
 });
 
@@ -70,8 +70,8 @@ $("#eft_btn").on("click", function () {
 $("#card_btn").on("click", function () {
   $("#card_modal_container").css("display", "flex");
   setTimeout(() => {
-      $("#card_modal_container").addClass("center-modal-container-active");
-      $("#card_center_modal").addClass("center-modal-active");
+    $("#card_modal_container").addClass("center-modal-container-active");
+    $("#card_center_modal").addClass("center-modal-active");
   }, 1);
 });
 
@@ -79,8 +79,8 @@ $("#card_btn").on("click", function () {
 $("#auto_topup_btn").on("click", function () {
   $("#auto_topup_modal_container").css("display", "flex");
   setTimeout(() => {
-      $("#auto_topup_modal_container").addClass("center-modal-container-active");
-      $("#auto_topup_center_modal").addClass("center-modal-active");
+    $("#auto_topup_modal_container").addClass("center-modal-container-active");
+    $("#auto_topup_center_modal").addClass("center-modal-active");
   }, 1);
 });
 
@@ -91,71 +91,99 @@ $("#auto_topup_btn").on("click", function () {
 
 // STATUS PAGE
 function countChars(obj) {
-    $("#CharCount").text(obj.value.length + "/65");
+  $("#CharCount").text(obj.value.length + "/65");
+}
+
+let merchantMsgIn = $('#merchant-msg-input');
+let merchantMsgOut = $('#merchant-msg-output');
+
+function updateMsg() {
+  if (merchantMsgIn.val() === "") {
+    merchantMsgOut.text("Delivery updates for your order #23910");
+  } else {
+    merchantMsgOut.text(merchantMsgIn.val());
   }
-  
-  let merchantMsgIn = $('#merchant-msg-input');
-  let merchantMsgOut = $('#merchant-msg-output');
-  
-  function updateMsg() {
-    if (merchantMsgIn.val() === "") {
-      merchantMsgOut.text("Delivery updates for your order #23910");
-    } else {
-      merchantMsgOut.text(merchantMsgIn.val());
-    }
+}
+
+// Profanity filter
+function profanityCheck() {
+  let merchantMsgValue = $('#merchant-msg-input').val();
+  let profanity = /faggot|nigga|nigger|arse|wanker|retard|dick|crap|shit|fuck|ass|penis|vagina|porn|pussy|cock|testicles|bitch|twat|cunt|slut|whore|poes|naai/;
+
+  if (merchantMsgValue.match(profanity)) {
+    merchantMsgValue = merchantMsgValue.replace(profanity, '');
+    $('#merchant-msg-input').val(merchantMsgValue);
   }
-  
-  // Profanity filter
-  function profanityCheck() {
-    let merchantMsgValue = $('#merchant-msg-input').val();
-    let profanity = /faggot|nigga|nigger|arse|wanker|retard|dick|crap|shit|fuck|ass|penis|vagina|porn|pussy|cock|testicles|bitch|twat|cunt|slut|whore|poes|naai/;
-  
-    if (merchantMsgValue.match(profanity)) {
-      merchantMsgValue = merchantMsgValue.replace(profanity, '');
-      $('#merchant-msg-input').val(merchantMsgValue);
-    }
-  }
-  
-  merchantMsgIn.on('keyup', profanityCheck);
-  merchantMsgIn.on('keyup', updateMsg);
-  
+}
+
+merchantMsgIn.on('keyup', profanityCheck);
+merchantMsgIn.on('keyup', updateMsg);
+
+// center modal tabs
+const tabs = document.querySelectorAll(".modal-tab");
+const tabContent = document.querySelectorAll(".modal-tab-content");
+let tabNo = 0;
+let contentNo = 0;
+tabs.forEach((tab) => {
+  tab.dataset.id = tabNo;
+  tabNo++;
+  tab.addEventListener("click", function () {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    this.classList.add("active");
+    tabContent.forEach((content) => {
+      content.scrollTop = 0;
+      content.classList.add("hidden");
+      if (content.dataset.id === tab.dataset.id) {
+        content.classList.remove("hidden");
+      }
+    });
+  });
+});
+
+tabContent.forEach((content) => {
+  content.dataset.id = contentNo;
+  contentNo++;
+});
+
 // ACCESSIBILITY
 // add shortcut modal
 $("#add_shortcut_btn").on("click", function () {
-    $("#shortcut_modal_container").css("display", "flex");
-    setTimeout(() => {
-        $("#shortcut_modal_container").addClass("center-modal-container-active");
-        $("#shortcut_center_modal").addClass("center-modal-active");
-    }, 1);
+  $("#shortcut_modal_container").css("display", "flex");
+  setTimeout(() => {
+    $("#shortcut_modal_container").addClass("center-modal-container-active");
+    $("#shortcut_center_modal").addClass("center-modal-active");
+  }, 1);
 });
 
 /* add zone template */
 $("#add_zone_btn").on("click", function () {
-    $("#zone_modal_container").css("display", "flex");
-    setTimeout(() => {
-        $("#zone_modal_container").addClass("center-modal-container-active");
-        $("#zone_center_modal").addClass("center-modal-active");
-    }, 1);
+  $("#zone_modal_container").css("display", "flex");
+  setTimeout(() => {
+    $("#zone_modal_container").addClass("center-modal-container-active");
+    $("#zone_center_modal").addClass("center-modal-active");
+  }, 1);
 });
 
 // CARRIERS
 // carrier config modal
 function showCarrierConfigModal() {
-    $("#carrier_modal_container").css("display", "flex");
-    $(".center-modal-body").scrollTop(0);
-    $("#s_modal").css("display", "flex");
-    setTimeout(() => {
-        $("#carrier_modal_container").addClass("center-modal-container-active");
-        $("#carrier_center_modal").addClass("center-modal-active");
-    }, 1);
+  $("#carrier_modal_container").css("display", "flex");
+  $(".center-modal-body").scrollTop(0);
+  $("#s_modal").css("display", "flex");
+  setTimeout(() => {
+    $("#carrier_modal_container").addClass("center-modal-container-active");
+    $("#carrier_center_modal").addClass("center-modal-active");
+  }, 1);
 }
 
 // fastway modal -- update this to use the trigger below and one global script for the modal.
 $("#fastway_config_btn").on("click", function () {
   $("#fastway_modal_container").css("display", "flex");
   setTimeout(() => {
-      $("#fastway_modal_container").addClass("center-modal-container-active");
-      $("#fastway_center_modal").addClass("center-modal-active");
+    $("#fastway_modal_container").addClass("center-modal-container-active");
+    $("#fastway_center_modal").addClass("center-modal-active");
   }, 1);
 });
 
@@ -165,52 +193,52 @@ $("[carrier='yango']").on("click", showCarrierConfigModal);
 // TEAM
 // add team member modal
 $("#add_team_btn").on("click", function () {
-    $("#team_modal_container").css("display", "flex");
-    setTimeout(() => {
-        $("#team_modal_container").addClass("center-modal-container-active");
-        $("#team_center_modal").addClass("center-modal-active");
-    }, 1);
+  $("#team_modal_container").css("display", "flex");
+  setTimeout(() => {
+    $("#team_modal_container").addClass("center-modal-container-active");
+    $("#team_center_modal").addClass("center-modal-active");
+  }, 1);
 });
 
 // toggle accordions on accordion-header click
 $('.accordion-header').on('click', function () {
-    $(this).parent().toggleClass('open');
-    $(this).find('.accordion-arrow').toggleClass('active');
-    $(this).siblings('.accordion-body').toggleClass('active');
+  $(this).parent().toggleClass('open');
+  $(this).find('.accordion-arrow').toggleClass('active');
+  $(this).siblings('.accordion-body').toggleClass('active');
 });
 
 // modal header shadow
 $(".center-modal-body").scroll(function () {
-    var scroll = $(this).scrollTop();
-    $(".center-modal-header").toggleClass("center-modal-header-active", scroll > 0);
-  });
+  var scroll = $(this).scrollTop();
+  $(".center-modal-header").toggleClass("center-modal-header-active", scroll > 0);
+});
 
 // btn loader
-$("#send_invite_btn").on("click", function(e) {
-    if (e.target !== this) {
-        $("#send_invite_btn").addClass("btn-p-loading");
-        $("#send_invite_loader").addClass("btn-p-loader-active");
-    }
+$("#send_invite_btn").on("click", function (e) {
+  if (e.target !== this) {
+    $("#send_invite_btn").addClass("btn-p-loading");
+    $("#send_invite_loader").addClass("btn-p-loader-active");
+  }
 });
 
 // FLEET
 // add driver modal
 $("#add_driver_btn").on("click", function () {
-    $("#driver_modal_container").css("display", "flex");
-    setTimeout(() => {
-        $("#driver_modal_container").addClass("center-modal-container-active");
-        $("#driver_center_modal").addClass("center-modal-active");
-    }, 1);
+  $("#driver_modal_container").css("display", "flex");
+  setTimeout(() => {
+    $("#driver_modal_container").addClass("center-modal-container-active");
+    $("#driver_center_modal").addClass("center-modal-active");
+  }, 1);
 });
 
 // TAGS
 // add tags modal
 $("#add_tags_btn").on("click", function () {
-    $("#tags_modal_container").css("display", "flex");
-    setTimeout(() => {
-        $("#tags_modal_container").addClass("center-modal-container-active");
-        $("#tags_center_modal").addClass("center-modal-active");
-    }, 1);
+  $("#tags_modal_container").css("display", "flex");
+  setTimeout(() => {
+    $("#tags_modal_container").addClass("center-modal-container-active");
+    $("#tags_center_modal").addClass("center-modal-active");
+  }, 1);
 });
 
 // INTEGRATIONS
@@ -285,60 +313,60 @@ trigger.on("mouseout", hideBtnTooltip);
 // OTHER
 // side nav modal
 function openSideMenu() {
-    $("#side-menu").css("left", "0%");
+  $("#side-menu").css("left", "0%");
+  $("#overlay").css({
+    transitionTimingFunction: "ease", transitionDuration: "0ms", zIndex: "1000"
+  });
+  setTimeout(() => {
     $("#overlay").css({
-        transitionTimingFunction: "ease", transitionDuration: "0ms", zIndex: "1000"
+      transitionDuration: "250ms", opacity: "100%"
     });
-    setTimeout(() => {
-        $("#overlay").css({
-            transitionDuration: "250ms", opacity: "100%"
-        });
-    }, 10);
+  }, 10);
 }
 
 function closeSideMenu() {
-    $("#side-menu").css("left", "-370px");
-    $("#overlay").css("opacity", "0%");
-    setTimeout(() => {
-        $("#overlay").css("zIndex", "-1");
-    }, 10);
+  $("#side-menu").css("left", "-370px");
+  $("#overlay").css("opacity", "0%");
+  setTimeout(() => {
+    $("#overlay").css("zIndex", "-1");
+  }, 10);
 }
 
 // hide all modals
 function hideAllCenterModal(e) {
-    if (e.target == this) {
-        $(".center-modal-container").removeClass("center-modal-container-active");
-        $(".center-modal").removeClass("center-modal-active");
-        setTimeout(() => {
-            $(".center-modal-container").css("display", "none");
-        }, 120);
-    }
+  if (e.target == this) {
+    $(".center-modal-container").removeClass("center-modal-container-active");
+    $(".center-modal").removeClass("center-modal-active");
+    setTimeout(() => {
+      $(".center-modal-container").css("display", "none");
+    }, 120);
+  }
 }
 $(".close-modal-btn, .center-modal-container").on("click", hideAllCenterModal);
 
 // context menus
 $(document).ready(function () {
-    $(".context-menu-btn").on("click", function (e) {
-        const contextMenuModal = $(this).next(".context-menu-modal");
-  
-        if (e.target.closest(".context-menu-btn")) {
-            if (contextMenuModal.hasClass("active")) {
-                contextMenuModal.removeClass("active");
-                setTimeout(() => contextMenuModal.css("display", "none"), 150);
-            } else {
-                $(".context-menu-modal").removeClass("active");
-                contextMenuModal.css("display", "block");
-                setTimeout(() => contextMenuModal.addClass("active"), 1);
-            }
-        }
-    });
-  
-    $(document).on("click", function (e) {
-        const contextMenuModal = $(".context-menu-modal");
-  
-        if (!e.target.closest(".context-menu-btn")) {
-            contextMenuModal.removeClass("active");
-            setTimeout(() => contextMenuModal.css("display", "none"), 150);
-        }
-    });
+  $(".context-menu-btn").on("click", function (e) {
+    const contextMenuModal = $(this).next(".context-menu-modal");
+
+    if (e.target.closest(".context-menu-btn")) {
+      if (contextMenuModal.hasClass("active")) {
+        contextMenuModal.removeClass("active");
+        setTimeout(() => contextMenuModal.css("display", "none"), 150);
+      } else {
+        $(".context-menu-modal").removeClass("active");
+        contextMenuModal.css("display", "block");
+        setTimeout(() => contextMenuModal.addClass("active"), 1);
+      }
+    }
   });
+
+  $(document).on("click", function (e) {
+    const contextMenuModal = $(".context-menu-modal");
+
+    if (!e.target.closest(".context-menu-btn")) {
+      contextMenuModal.removeClass("active");
+      setTimeout(() => contextMenuModal.css("display", "none"), 150);
+    }
+  });
+});
